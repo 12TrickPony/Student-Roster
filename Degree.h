808065ac4d2cc1611d66274d93fe16fa95064369
Michelle Renee <36307403+12TrickPony@users.noreply.github.com>
2022-02-21 15:52:22 -0800

    Update Degree.h

--- Degree.h
+++ Degree.h
@@ -2,6 +2,6 @@
 #include <string>
 #include <iostream>
 
-// TASK C
+
 enum class DegreeProgram { SECURITY, NETWORK, SOFTWARE };
 static const std::string DegreeTypeStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
